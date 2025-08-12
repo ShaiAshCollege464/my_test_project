7ca473be3c6f5512ceb960ebda2f272daa609087
Daniela <daniela>
2025-08-12 20:51:51 +0300

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("This is from Daniela!");
         System.out.println("Hello!");
     }
 }
